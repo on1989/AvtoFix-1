05e1b9ed7245810a4fa84dc9dafe2b04af564958
Alina Devonina <alina.devonina@gmail.com>
2016-09-05 18:08:29 +0300

    Create new page orders-page.html, add jquery-slider

--- js/common.js
+++ js/common.js
@@ -216,6 +216,48 @@ $(document).mouseup(function (e)  {
 		folder.hide();
 	}
 });
+// ПОЛЗУНОК
+  $("#slider").slider({
+    min: 100,
+    max: 30000,
+    values: [100,5000],
+    range: true,
+    stop: function(event, ui) {
+        $("input#minCost").val($("#slider").slider("values",0));
+        $("input#maxCost").val($("#slider").slider("values",1));
+    },
+    slide: function(event, ui){
+        $("input#minCost").val($("#slider").slider("values",0));
+        $("input#maxCost").val($("#slider").slider("values",1));
+    }
+
+});
+  $("input#minCost").change(function(){
+    var value1=$("input#minCost").val();
+    var value2=$("input#maxCost").val();
+ 
+    if(parseInt(value1) > parseInt(value2)){
+        value1 = value2;
+        $("input#minCost").val(value1);
+    }
+    $("#slider").slider("values",0,value1);
+});
+ 
+     
+$("input#maxCost").change(function(){
+    var value1=$("input#minCost").val();
+    var value2=$("input#maxCost").val();
+     
+    if (value2 > 30000) { value2 = 30000; $("input#maxCost").val(30000)}
+ 
+    if(parseInt(value1) > parseInt(value2)){
+        value2 = value1;
+        $("input#maxCost").val(value2);
+    }
+    $("#slider").slider("values",1,value2);
+});
+
+
 });
 
 // END TEXT HIDE
